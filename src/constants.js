3beb044205efeb5b9ffecfe24d013462345601fe
R2K RAHUL <123983265+Rahul-B47@users.noreply.github.com>
2025-07-18 20:38:11 +0530

    Update logos and constants

--- src/constants.js
+++ src/constants.js
@@ -49,7 +49,7 @@ import canaraLogo from './assets/education_logo/canaraLogo.png';
 
 //Certification Section Logo's
 import simplilearnLogo from "./assets/certifications_logo/simplilearn_logo.png";
-import nptelLogo from "./assets/certifications_logo/nptel_logo.png";
+// import nptelLogo from "./assets/certifications_logo/nptel_logo.png";
 import powerBiLogo from "./assets/certifications_logo/powerbi_workshop_logo.png";
 
 
@@ -142,18 +142,18 @@ export const SkillsInfo = [
 ];
 
 export const certifications = [
-  {
-    id: 1,
-    title: "Programming in Java",
-    issuer: "NPTEL",
-    date: "May 2024",
-    desc: "Completed an NPTEL-certified course on Java programming, gaining in-depth knowledge of OOPs, exception handling, multithreading, and Java libraries.",
-    skills: ["Java", "OOP", "Exception Handling", "Multithreading"],
-    keywords: ["NPTEL", "Certified", "Java SE"],
-    image:nptelLogo, // Place your image in public/certificates
-    certificateLink: "https://archive.nptel.ac.in/content/noc/NOC24/SEM1/Ecertificates/106/noc24-cs43/Course/NPTEL24CS43S96140045130652942.pdf", // Replace with real link if available
-    downloadLink: "/certificates/nptel_java.pdf", // Replace with actual file
-  },
+  // {
+  //   id: 1,
+  //   title: "Programming in Java",
+  //   issuer: "NPTEL",
+  //   date: "May 2024",
+  //   desc: "Completed an NPTEL-certified course on Java programming, gaining in-depth knowledge of OOPs, exception handling, multithreading, and Java libraries.",
+  //   skills: ["Java", "OOP", "Exception Handling", "Multithreading"],
+  //   keywords: ["NPTEL", "Certified", "Java SE"],
+  //   image:nptelLogo, // Place your image in public/certificates
+  //   certificateLink: "https://archive.nptel.ac.in/content/noc/NOC24/SEM1/Ecertificates/106/noc24-cs43/Course/NPTEL24CS43S96140045130652942.pdf", // Replace with real link if available
+  //   downloadLink: "/certificates/nptel_java.pdf", // Replace with actual file
+  // },
   {
     id: 2,
     title: "JavaScript for Beginners",
